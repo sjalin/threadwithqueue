17ed4028f2424694de55efeb5c1b12f5c421d46e
sjalin <sebastian.sjalin@gmail.com>
2022-09-03 16:05:12 +0200

    Initial version, no bells and/or whistles!

---
+++ README.md
@@ -0,0 +1,4 @@
+# thread-with-queue
+Just a basic thread with a simple queue used for messages
+
+Messages are tuples with i guess plain text for the command and some kind of payload...
---
+++ threadwithqueue.py
@@ -0,0 +1,40 @@
+import logging
+from queue import SimpleQueue
+from threading import Thread
+
+import _queue
+
+
+class ThreadWithQueue(Thread):
+    def __init__(self):
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.log.info(f'Init')
+        self._message_queue: SimpleQueue = SimpleQueue()
+
+        super().__init__()
+
+    def run(self):
+        self.log.info(f'Run')
+        while True:
+            msg = self._get_message()
+            if msg:
+                if msg[0] == 'DIE':
+                    self.log.info(f'Stopping')
+                    break
+                else:
+                    self._handle_message(msg)
+
+    def _handle_message(self, msg):
+        raise NotImplementedError('Implement _handle_message')
+
+    def get_message_queue(self):
+        return self._message_queue
+
+    def _get_message(self, timeout=6):
+        message = None
+        try:
+            message = self._message_queue.get(timeout=timeout)
+            self.log.debug(f'Got message - {message}')
+        except _queue.Empty:
+            pass
+        return message
